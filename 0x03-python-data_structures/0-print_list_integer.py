b2eef45e73fdb6aeaa55c831299db31a4b775a3a
Beza29 <bezaasrat29@gmail.com>
2022-12-05 07:45:37 +0000

    phyton

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print all integers of a list
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+
+def print_list_integer(my_list=[]):
+    """Prints all intergers of a list
+    Args:
+      my_list: the list argument whose items will be printed
+    """
+
+    # Iterate through list
+    for integer in my_list:
+        print("{:d}".format(integer))
