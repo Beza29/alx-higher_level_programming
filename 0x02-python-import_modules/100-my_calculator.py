f85d0b88e1aa92b3ec540b31a3baaa8796e79c25
Beza29 <bezaasrat29@gmail.com>
2022-12-06 08:24:08 +0000

    sdpora

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to import the variable a from the file
-# variable_load_5.py and handles basic operations
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
+from sys import argv, exit
+from calculator_1 import add, sub, mul, div
 
-# Import sys module to access argv
-from sys import argv
 
-# Import the functions: add, sub, mul, div
-from main import add, sub, mul, div
-
-operators = ["+", "-", "*", "/"]
-arg_len = len(argv)
-
-if __name__ == "__main__":
-    if (arg_len - 1) < 3:
+def calculate(arr):
+    if len(arr) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif argv[2] not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
     else:
-        a, b = int(argv[1]), int(argv[3])
-        if argv[2] == "+":
-            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-        elif argv[2] == "-":
-            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-        elif argv[2] == "*":
-            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+        op = (arr[1])
+        if op not in ['+', '-', '*', '/']:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        a = int(arr[0])
+        b = int(arr[2])
+        print(a, op, b, "=", end=" ")
+        if op == '+':
+            print(add(a, b))
+        elif op == '-':
+            print(sub(a, b))
+        elif op == '*':
+            print(mul(a, b))
         else:
-            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+            print(div(a, b))
+    exit(0)
+
+
+if (__name__ == '__main__'):
+    calculate(argv[1:])
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to import the variable a from the file
-# variable_load_5.py and prints its value.
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
 
-# Import a from file
-from main import a
 
-# This code should not run when this file is imported
-if __name__ == "__main__":
-    print(a)
+def output():
+    from variable_load_5 import a
+    return a
+
+
+if (__name__ == '__main__'):
+    print(output())
