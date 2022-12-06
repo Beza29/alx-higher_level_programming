fc62db791316ab1723665498f1e23e3ee2d55478
Beza29 <bezaasrat29@gmail.com>
2022-12-06 08:09:55 +0000

    phytone

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the number of commandline arguments
+# and the list of its arguments
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
+# import sys module to access argv
+from sys import argv
+
+# Determine the length of argv excluding item at index 0
+argv_length = len(argv) - 1
+
+# This code should not run when this file is imported
 if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
-
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+    # Print out different statements based on length
+    if argv_length > 1:
+        print("{:d} arguments:".format(argv_length))
+    elif argv_length == 0:
+        print("{:d} arguments.".format(argv_length))
     else:
-        print("{} arguments:".format(i))
+        print("{:d} argument:".format(argv_length))
 
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+    # Loop through and print out item and it's index
+    for arg_item in argv:
+        if argv.index(arg_item) == 0:
+            continue
+        print("{:d}: {}".format(argv.index(arg_item), arg_item))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print the result of the addition of all
+# commandline arguments
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
 
+# Import sys module to access argv
+from sys import argv
+
+# Initialize sum to zero
+sum = 0
+
+# This code should not run when this file is imported
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    # Iterate through the arguments
+    for nums in argv:
+
+        # Skip the first argument
+        if argv.index(nums) == 0:
+            continue
+        # Sum all other arguments
+        sum += int(nums)
+
+    # Print out sum
+    print(sum)
