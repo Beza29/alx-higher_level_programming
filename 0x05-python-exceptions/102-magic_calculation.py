1b0ab426adb9e40d0a661bd12d00ea8f02985efd
Beza29 <bezaasrat29@gmail.com>
2022-12-20 18:51:30 +0000

    asdgti

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """function that does exactly the same as the given Python bytecode.
+    Args:
+        a: integer
+        b: integer
+    """
+    result = 0
+
+    for num in range(1, 3):
+        try:
+            if num > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / num
+        except Exception:
+            result = b + a
+            break
+    return 
